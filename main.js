3e9c9a31ca1887d6fc853fed996d93088854e79d
Antoni Lueddeke <123427446+alueddeke@users.noreply.github.com>
2023-03-12 15:17:28 -0400

    added sizing and error message

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
-let board = document.querySelector(".board");
-//applying the css to the board directly from js
-//makes repeated Rows
-//
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+let color = "black";
 
-for (let i = 0; i <= 256; i++) {
-  let square = document.createElement("div");
-  square.style.backgroundColor = "grey";
-  square.style.border = "solid black";
-  board.insertAdjacentElement("beforeend", square);
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  //applying the css to the board directly from js
+  //makes repeated Rows
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", () => {
+      square.style.background = color;
+    });
+    /*square.addEventListener("dragstart", () => {
+      square.addClassList.add("dragging");
+    });*/
+    square.style.backgroundColor = "grey";
+    square.style.border = "1px solid black";
+
+    board.insertAdjacentElement("beforeend", square);
+  }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    console.error("Invalid number of squares");
+  }
 }
