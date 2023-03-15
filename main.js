1330d54a07f52628cfa9725139fb6f5237c2045c
Antoni Lueddeke <123427446+alueddeke@users.noreply.github.com>
2023-03-15 13:28:40 -0400

    button functionality, click function

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let color = "black";
+let click = false;
 
 function populateBoard(size) {
   let board = document.querySelector(".board");
@@ -12,13 +13,11 @@ function populateBoard(size) {
   let amount = size * size;
   for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
-    square.addEventListener("mouseover", () => {
-      square.style.background = color;
-    });
+    square.addEventListener("mouseover", colorSquare);
     /*square.addEventListener("dragstart", () => {
       square.addClassList.add("dragging");
     });*/
-    square.style.backgroundColor = "grey";
+    square.style.backgroundColor = "white";
     square.style.border = "1px solid black";
 
     board.insertAdjacentElement("beforeend", square);
@@ -34,3 +33,35 @@ function changeSize(input) {
     console.error("Invalid number of squares");
   }
 }
+
+function colorSquare() {
+  //only color a square if click is true
+  if (click) {
+    //everytime we make a square, we make a function dedicated to that square
+    //using "this"
+    if (color === "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
+    }
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function reset() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "white"));
+}
+
+//listen for clicks in the body
+document.querySelector("body").addEventListener("click", () => {
+  //if click is true, set it to false
+  //if it is false, set it to true
+  //value if click, will be whatever click is not
+
+  click = !click;
+});
